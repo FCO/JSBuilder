85bb92e911a18f60f457bf7e6894f369923ed6f9
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2011-11-18 12:59:18 -0200

    Constructor and setter pars for IOC working

--- js/ObjectLifeCicleManager.js
+++ js/ObjectLifeCicleManager.js
@@ -12,18 +12,23 @@ ObjectLifeCicleManager.prototype = {
    instanciate: function(class_name, prop) {
       var pars = [];
       if(prop != null && prop.constructor != null){
-	 if(typeof(prop.constructor) == "string") {
-	       pars.push(prop.constructor);
-	 } else {
+         if(prop.constructor.length == null) {
+               pars.push(JSON.stringify(this.construct_obj(prop.constructor)));
+         } else {
             for(var i = 0; i < prop.constructor.length; i++) {
-	       pars.push(prop.constructor[i]);
-	    }
-	 }
+               pars.push(JSON.stringify(this.construct_obj(prop.constructor[i])));
+            }
+         }
       }
+
       var new_obj = eval("new " + class_name + "(" + pars.join(", ") + ")");
+
       if(prop != null && prop.setter != null){
          for(var attr in prop.setter) {
-	   eval("new_obj." + attr + "(" + prop.setter.attr + ")");
+	   if(new_obj[attr] == null){
+	     throw "'" + attr + "' is not a function, please review your IOC configuration";
+	   }
+	   eval("new_obj." + attr + "("+ JSON.stringify(this.construct_obj(prop.setter[attr])) + ")");
 	 }
       }
       return new_obj;
@@ -31,6 +36,9 @@ ObjectLifeCicleManager.prototype = {
    get_obj: function(name) {
       if(this.conf[name] == null) throw "Class '" + name + "' not configured";
       var hash = this.conf[name];
+      return this.construct_obj(hash);
+   },
+   construct_obj: function(hash) {
       for(var key in hash) {
          if(key == "singleton") {
             return this.get_instance(hash["singleton"], hash);
@@ -38,6 +46,8 @@ ObjectLifeCicleManager.prototype = {
             return this.instanciate(hash["new"], hash);
          } else if(key == "string") {
             return hash["string"];
+         } else if(key == "bool") {
+            return hash["bool"] == "true";
          }
       }
    },
--- projects/Test03/Every.js
+++ projects/Test03/Every.js
@@ -1,7 +1,17 @@
-function Every() { }
+function Every(log) {
+   this.log = log == "log";
+   if(this.log) {
+      console.log("Instanciating new Every object");
+   }
+}
 
 Every.prototype = {
    toString: function() {
+      if(this.log)
+         console.log("I should be logging!");
       return $IOC.get_obj("who");
-   }
+   },
+   do_log: function(msg) {
+     this.log = msg;
+   },
 };
